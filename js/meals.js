de296ec50d39177581ffe14ab37e2f82cf88a153
Benjamin Michaelis <benjamin@michaelis.net>
2022-01-25 22:37:52 -0800

    Adding more recipes and small bug fixes/improvements

--- js/meals.js
+++ js/meals.js
@@ -1,22 +1,56 @@
+// 0 - Easy < 30 min
+// 1 - Medium 30min-60min
+// 2 - Hard 60 min <
+// 3 - Pickup
 window["Village Meal Generator v1.0"] = [
   {
     "label": "BYOC (Bring your own cereal night)",
-    "level": 1,
+    "level": 0,
     "notes": "Everyone brings there favorite type of cereal!"
   },
   {
     "label": "Lasagna",
     "level": 2,
-    "notes": ""
+    "notes": "https://www.spendwithpennies.com/wprm_print/recipe/145181"
   },
   {
     "label": "Pizza",
-    "level": 1,
+    "level": 3,
     "notes": ""
   },
   {
     "label": "Pancake Night",
     "level": 0,
     "notes": "Make Pancakes!!"
+  },
+  {
+    "label": "Nacho Bar",
+    "level": 1,
+    "notes": "Little bit of prep but easy from there!"
+  },
+  {
+    "label": "Grilled Cheese and Tomato Soup",
+    "level": 0,
+    "notes": "A great comfort meal"
+  },
+  {
+    "label": "Spaghetti",
+    "level": 0,
+    "notes": "Cheap and filling meal!"
+  },
+  {
+    "label": "Burrito Bowls",
+    "level": 1,
+    "notes": "Rice, meat, and beans!"
+  },
+  {
+    "label": "Tacos",
+    "level": 1,
+    "notes": ""
+  },
+  {
+    "label": "Macaroni",
+    "level": 0,
+    "notes": ""
   },
 ];
