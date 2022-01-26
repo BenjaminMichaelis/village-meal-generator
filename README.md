02d5e3ed85170d503be4827ee8b84f8208246723
Benjamin Michaelis <benjamin@michaelis.net>
2022-01-25 16:45:33 -0800

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Village Meal Generator
---
+++ js/challenges.js
@@ -0,0 +1,22 @@
+window["Village Meal Generator v1.0"] = [
+  {
+    "label": "BYOC (Bring your own cereal night)",
+    "level": 1,
+    "notes": ""
+  },
+  {
+    "label": "Lasagna",
+    "level": 2,
+    "notes": ""
+  },
+  {
+    "label": "Pizza",
+    "level": 1,
+    "notes": ""
+  },
+  {
+    "label": "Pancake Night",
+    "level": 0,
+    "notes": "Make Pancakes!!"
+  },
+];
